e644f090cf666f0a6882015fa50726e7d27d5982
derbie <ideahub-derbie@outlook.com>
2024-07-02 08:15:23 +0800

    append

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,35 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'DMS Starlight',
+			favicon: '/favicon.ico',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				// github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Start Here',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ label: 'Installations', link: '/starter/installation/' },
+						{ label: 'DMS Overview', link: '/starter/overview/' },
+					],
+				},
+				{
+					label: 'Modules',
+					items: [
+						{ label: 'Dashboard', link: '/modules/dashboard/' },
+						{ label: 'Collections', link: '/modules/collections/' },
+						{ label: 'Contracts', link: '/modules/contracts/' },
+						{ label: 'Customers', link: '/modules/customers/' },
+						{ label: 'Inventory', link: '/modules/inventory/' },
+						{ label: 'Lots', link: '/modules/lots/' },
+						{ label: 'Make a Payment', link: '/modules/make-a-payment/' },
+						{ label: 'Payments', link: '/modules/payments/' },
+						{ label: 'Test Drives', link: '/modules/test-drives/' },
+						{ label: 'Vendors', link: '/modules/vendors/' },
+						{ label: 'Vehicles', link: '/modules/vehicles/' },
+						{ label: 'Recon', link: '/modules/recon/' },
+						{ label: 'Reports', link: '/modules/reports/' },
 					],
 				},
 				{
@@ -22,6 +41,24 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 			],
+			// Set English as the default language for this site.
+			defaultLocale: 'en',
+			locales: {
+				// English docs in `src/content/docs/en/`
+				en: {
+					label: 'English (US)',
+				},
+				// Simplified Chinese docs in `src/content/docs/zh-cn/`
+				'zh-cn': {
+					label: '简体中文',
+					lang: 'zh-CN',
+				},
+				// Arabic docs in `src/content/docs/ar/`
+				ar: {
+					label: 'العربية',
+					dir: 'rtl',
+				},
+			},
 		}),
 	],
 });
